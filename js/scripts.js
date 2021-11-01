ecde4192b561b654caabe895194774bf437219c6
LaMellUI <91955313+LaMellUI@users.noreply.github.com>
2021-11-01 17:17:27 +0000

    fix errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
-console.log("Hello! Welcome to my UX / UI Portfolio Site")
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += 'responsive';
   } else {
     x.className = 'navtoggle';
   }
